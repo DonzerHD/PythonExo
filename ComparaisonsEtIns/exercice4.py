bf482eb017fb03c7039f72f69a71d5ada5f20b12
Thomas59 <thomas.lemay59@outlook.fr>
2023-01-06 15:35:02 +0100

    py

---
+++ ComparaisonsEtIns/exercice4.py
@@ -0,0 +1,7 @@
+lettre = input("Tape une lettre :")
+
+if lettre == 'a' or lettre == 'e' or lettre == 'i' or lettre == 'o' or lettre == 'u' or lettre == 'y':
+    print("c'est une voyelle")
+else:
+    print("C'est une consonne")
+
