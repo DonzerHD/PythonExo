75509cece206c81f16091944bd8c385e73758057
DonzerHD <thomas.lemay59@outlook.fr>
2023-01-23 22:21:21 +0100

    tri par insertion

---
+++ Algorithmes de tri/main.py
@@ -0,0 +1,16 @@
+
+
+def tri_par_insertion(l):
+    for i in range(1, len(l)):
+        cle = l[i]
+        j = i-1
+        print(f"i = {i} , cle = {cle} , j = {j}")
+        while j>=0 and cle < l[j]:
+            print(f"l[j] = {l[j]}")
+            l[j+1] = l[j]
+            j -= 1
+        l[j+1] = cle
+            
+l = [5,3,2,7]
+tri_par_insertion(l)
+print(l)
